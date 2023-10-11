a44b0ea6cb2ec830aa83d86758945c0c6c059e26
vnavani <vinita.navani@gmail.com>
2023-10-10 21:59:12 -0700

    react components

---
+++ client/src/App.jsx
@@ -0,0 +1,53 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+  
+    <ApolloProvider client={client}>
+   
+
+      
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>}/> 
+          <Route path="/login" element={<Login/>}/> 
+          <Route path="/signup" element={<Signup/>}/> 
+        </Routes>
+      </Router>
+
+        </ApolloProvider> 
+  );
+}
+
+export default App;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <>
+        <h1>
+            Login
+        </h1>
+        </>
+    )
+}
+export default Login;
