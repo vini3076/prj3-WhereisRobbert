48ec7f1663c6eb217c857296435304b447cfc412
ahermez <131738383+ahermez@users.noreply.github.com>
2023-10-12 18:39:55 -0700

    api syntax

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getCamps } from "../utils/API";
+
 
 const Home = () => {
+    useEffect(() => {
+        getCamps().then (response => response.json())
+        .then(data => console.log(data))
+    }, [])
     return (
         <>
         <h1>
