b380bb7f0bd388567db8d728352917a17cd2faf3
vnavani <vinita.navani@gmail.com>
2023-10-17 20:09:23 -0700

    server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ import path from 'path';
 import { authMiddleware } from './utils/auth.js'; 
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
