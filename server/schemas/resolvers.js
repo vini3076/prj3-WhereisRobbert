c41b9205070a638fc97fc5f73265432d9232c798
vnavani <vinita.navani@gmail.com>
2023-10-17 19:40:57 -0700

    heroku

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,8 +18,10 @@ const resolvers = {
       throw new AuthenticationError("Not logged in");
     },
     getCamps: async(parent, {searchString}) => {
-      console.log('here')
+      console.log(process.env.API_KEY)
+      console.log(searchString)
       const response = await fetch(`https://developer.nps.gov/api/v1/campgrounds?stateCode=CA&q=${searchString}&api_key=${process.env.API_KEY}`)
+      console.log(response)
 
 
         if (!response.ok) {
